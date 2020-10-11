caf7ae1c1b36e3b3cf22c405b86f961fb6752560
LEEJJ <LEEJJ@DESKTOP-EVRRJ6K>
2020-10-11 16:36:01 +0900

    jsp에 ModelMap 내리기

--- egovWeb/src/main/java/com/egovweb/main/web/MainController.java
+++ egovWeb/src/main/java/com/egovweb/main/web/MainController.java
@@ -1,14 +1,21 @@
 package com.egovweb.main.web;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 	
 	@RequestMapping(value="/main.do")
-	public String main() throws Exception{
+	public String main(ModelMap map) throws Exception{
 		System.out.println("mainController => main.do");
+		LocalDateTime now = LocalDateTime.now();
+		String nowDate = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		map.addAttribute("date", nowDate);
 		return "main/main";
 	}
 }
